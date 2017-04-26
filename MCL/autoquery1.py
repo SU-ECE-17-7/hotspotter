d580f412c2812d78e2b9cb880f6475daf70fe1d5
wellern <wellern@github.com>
2017-04-26 11:32:02 -0700

    autoquery in API

--- MCL/autoquery1.py
+++ MCL/autoquery1.py
@@ -6,38 +6,109 @@
 
 import numpy as np 
 import csv
-#maxMatches = 6 #decide how many to do 
+
+SAME_IMAGE_WEIGHT = .9
+SAME_SET_WEIGHT =  1
 
 def createMatrix(hs): #maybe get rid of *args and **kwargs everywhere
         size = hs.get_num_chips()
         Matrix = np.zeros((size,size))
-
-	for chip1 in hs.get_valid_cxs(): #iterate through all chips
-                ID = hs.cx2_cid(chip1)
-                res_dict = {}
-                res_dict = hs.query(ID)
-                results = res_dict.cx2_score #i think cx2_score
+        
+	for i in hs.get_valid_cxs(): #iterate through all chips
+                chip1 = hs.cx2_cid(i)
+		chip1Name = hs.cx2_gname(i)
+	        res_dict = {}
+                res_dict = hs.query(chip1)
+                results = res_dict.cx2_score 
                 div = max(results)
                 if div > 0.0:
-                        modResults = [num/div for num in results]
-        
-                temp = np.nonzero(modResults)
-                matches = temp[0]
-
-
-                
-                for score in matches:
-                        if Matrix[ID-1][score -1] == 0.0:
-                                Matrix[ID-1][col-1] = temp[score]
-                                Matrix[col-1][ID-1] = temp[score]
-                        else:
-                                Matrix[ID-1][col-1] = (temp[score] + Matrix[ID-1][col-1])/2
-				Matrix[col-1][ID-1] = (temp[score] + Matrix[col-1][ID-1])/2
-			
-		
+                        results = [num/div for num in results]
+ 		for j in hs.get_valid_cxs():
+			chip2 = hs.cx2_cid(j)
+			chip2Name = hs.cx2_gname(j)
+			if chip1!=chip2:
+				if sameImage(chip1Name, chip2Name):
+					Matrix[i][j] = SAME_IMAGE_WEIGHT
+					Matrix[j][i] = SAME_IMAGE_WEIGHT
+			        elif sameSet(chip1Name, chip2Name) and Matrix[i][j] == 0.0:
+                                        Matrix[i][j] = (results[j] + SAME_SET_WEIGHT)/2
+				        Matrix[j][i] = (results[j] + SAME_SET_WEIGHT)/2
+				elif sameSet(chip1Name, chip2Name) and Matrix[i][j] != 0.0:
+					Matrix[i][j] = (((Matrix[i][j] + results[j])/2) + SAME_SET_WEIGHT)/2
+					Matrix[j][i] = (((Matrix[j][i] + results[j])/2) + SAME_SET_WEIGHT)/2
+				elif not sameSet(chip1Name, chip2Name) and Matrix[i][j] == 0.0:
+					Matrix[i][j] = results[j] 
+					Matrix[j][i] = results[j]
+				else:
+					Matrix[i][j] = (Matrix[i][j] + results[j])/2
+					Matrix[j][j] = (Matrix[j][i] + results[j])/2
+                                        
 	return Matrix
 	
 
+
+
+
+
+def sameSet(chip1, chip2):
+        
+        #if in same location
+        if getLoc(chip1) == getLoc(chip2):
+                #if on same date
+                if getDate(chip1) == getDate(chip2):
+                        #print "same location and date"
+                        
+                        #if close time
+                        if np.abs(getTime(chip1) - getTime(chip2)) <= secsThresh:
+                                #print "within secsThresh"
+                                return True
+                        else:
+                                #print "not within secsThresh"
+                                return False
+                        #elif one day apart but still less than 90 second:
+                        #    (getTime(chip1) <= secsThresh or
+                        #     getTime(chip1) > (secsInDay - secThresh)):
+                        #check dates still
+                        
+                else:
+                        #print "not same date"
+                        return False
+        else:
+                #print "not same location"
+                return False
+
+
+def getLoc(chip):
+        chipList = chip.split("__")
+        loc = (chipList[0] + "__" + chipList[1] +"__"+chipList[2])
+        return loc
+
+def getDate(chip):
+        date  = chip.split("__")[3]
+        return date
+
+#def getDateInDays(date):
+# figure out if stuff happened at end of a month (or year...)
+
+def getTime(chip):
+        chipList = chip.split("__")
+        timeStr = chipList[4]
+        
+        timeStr = timeStr.split("(")[0]
+        timeList = timeStr.split("-")
+        
+        time = int(timeList[0])*3600 + int(timeList[1])*60 + int(timeList[2])
+
+        return time
+
+	
+def sameImage(chip1, chip2):
+        
+	if chip1 == chip2:
+		return True
+	else:
+		return False
+        
 	
 def createFile(hs, Mat):
         size = hs.get_num_chips()
